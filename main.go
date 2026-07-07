e457de423eec720f3ffcc85e509f5db48b204d34
Deepak-S-M <deepakvnpy@gmail.com>
2026-07-07 14:42:01 +0530

    Graceful shutdown

--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"restaurant-order-management/config"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Restuarant Order Management API Starting...")
+	fmt.Println("Restaurant Order Management API Starting...")
 	config.ConnectDB()
 	SeedData()
 
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Recovery())
 	SetupRoutes(r)
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal("Failed to start server: ", err)
-	} else {
-		fmt.Println("Server running on port 8080")
+	addr := os.Getenv("SERVER_ADDRESS")
+	if addr == "" {
+		addr = ":8080"
 	}
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	// goroutine
+	go func() {
+		fmt.Printf("Server running on %s\n", addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Failed to start server: ", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	fmt.Println("\nShutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	fmt.Println("Server exited cleanly.")
 }
